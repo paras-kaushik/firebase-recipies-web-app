5beddc3bbd7ee3b90e0957b5c4bd1214523214ff
Paras Kaushik <parkaush1@WKMZT5811CC5.global.publicisgroupe.net>
2023-02-14 14:10:43 +0530

    auth added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import { useState } from 'react';
+import FirebaseAuthService from './FirebaseAuthService';
+import LoginForm from './components/LoginForm';
+
 import './App.css';
-import firebase from './FirebaseConfig';
+
 function App() {
+  const [user, setUser] = useState(null);
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser);
+
   return (
     <div className="App">
-      <div className='title-row'>
-        <h1 className='title'>
-          Firebase recicpies
-          Testing the workflow
-        </h1>
+      <div className="title-row">
+        <h1 className="title">Firebase Recipes</h1>
+        <LoginForm existingUser={user}></LoginForm>
       </div>
     </div>
   );
